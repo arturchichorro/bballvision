a054b2bd03c9095e7f538228cfc212ffe6191055
arturchichorro <arturchichorro@gmail.com>
2024-10-11 16:00:36 +0100

    Fixed bugs

--- bballvision.py
+++ bballvision.py
@@ -3,71 +3,13 @@ import cv2
 import math
 import numpy as np
 from collections import deque
-
-def distance(p1, p2):
-    """
-    Args:
-    p1 (x,y) and p2 (x,y)
-    """
-    return math.sqrt((p1[0] - p2[0])**2 + (p1[1] - p2[1])**2)
-
-def is_increasing_distances(point, points_array):
-    """
-    Args:
-    point (tuple): (x,y)
-    points_array (list): List of tuples [(x_1, y_1), (x_2, y_2), ...].
-
-    Returns:
-    bool: True if the distances are strictly increasing, False otherwise.
-    """
-
-    # Calculate distances between (x, y) and all points in the array
-    distances = [distance(point, (x_i, y_i)) for (x_i, y_i) in points_array]
-
-    # Check if distances are strictly increasing
-    for i in range(1, len(distances)):
-        if distances[i] <= distances[i - 1]:
-            return False
-    return True
-
-def is_ball_above_rim(ball, rim):
-    """
-    Args: 
-    ball (cx, cy, frame)
-    rim (x1, y1, x2, y2, frame)
-    """
-    return ball[1] < rim[1]
-
-
-def is_ball_below_rim(ball, rim):
-    """
-    Args: 
-    ball (cx, cy, frame)
-    rim (x1, y1, x2, y2, frame)
-    """
-    return ball[1] > rim[3]
-
-def is_made_shot(above_rim, below_rim, rim):
-    """
-    Args:
-    above_rim (cx, cy, frame)
-    below_rim (cx, cy, frame)
-    rim (x1, y1, x2, y2, frame)
-    """
-    x1, y1, x2 = rim[0], rim[1], rim[2]
-    cx1, cy1, cx2, cy2 = above_rim[0], above_rim[1], below_rim[0], below_rim[1]
-
-    m = (cy2-cy1)/(cx2-cx1)
-    b = cy1 - m*cx1
-    x = (y1 - b) / m
-
-    return x1 < x and x < x2
+from helper import is_increasing_distances, is_ball_below_rim, is_ball_above_rim, is_made_shot
 
 # Load Video
 video_path = 'input_vids/vid29.mp4'
 cap = cv2.VideoCapture(video_path)
 
-model = YOLO("best.pt")
+model = YOLO("bballvision.pt")
 
 # "made" class doesn't work very well
 classnames = ["ball", "made", "person", "rim", "shoot"]
@@ -161,9 +103,6 @@ while True:
     cv2.putText(img, f'Attempts: {str(total_attempts)}', (50, 50), cv2.FONT_HERSHEY_PLAIN, 2, (0, 255, 0), 3)
     cv2.putText(img, f'Made Shots: {total_made}', (50, 100), cv2.FONT_HERSHEY_PLAIN, 2, (0, 255, 0), 3)
 
-    # Blend the overlay onto the main frame
-    blended_img = cv2.addWeighted(img, 1.0, overlay, 1, 0)
-
     frame += 1
 
     # Adds circles on ball position every 5 frames
@@ -178,6 +117,9 @@ while True:
             cx, cy, pos_frame = pos
             if pos_frame % 5 == 0:
                 cv2.circle(overlay, (cx, cy), 5, (0, 0, 255), cv2.FILLED)
+    
+    # Blend the overlay onto the main frame
+    blended_img = cv2.addWeighted(img, 1.0, overlay, 1, 0)
 
     cv2.imshow("Image", blended_img)
 
---
+++ helper.py
@@ -0,0 +1,60 @@
+import math
+
+def distance(p1, p2):
+    """
+    Args:
+    p1 (x,y) and p2 (x,y)
+    """
+    return math.sqrt((p1[0] - p2[0])**2 + (p1[1] - p2[1])**2)
+
+def is_increasing_distances(point, points_array):
+    """
+    Args:
+    point (tuple): (x,y)
+    points_array (list): List of tuples [(x_1, y_1), (x_2, y_2), ...].
+
+    Returns:
+    bool: True if the distances are strictly increasing, False otherwise.
+    """
+
+    # Calculate distances between (x, y) and all points in the array
+    distances = [distance(point, (x_i, y_i)) for (x_i, y_i) in points_array]
+
+    # Check if distances are strictly increasing
+    for i in range(1, len(distances)):
+        if distances[i] <= distances[i - 1]:
+            return False
+    return True
+
+def is_ball_above_rim(ball, rim):
+    """
+    Args: 
+    ball (cx, cy, frame)
+    rim (x1, y1, x2, y2, frame)
+    """
+    return ball[1] < rim[1]
+
+
+def is_ball_below_rim(ball, rim):
+    """
+    Args: 
+    ball (cx, cy, frame)
+    rim (x1, y1, x2, y2, frame)
+    """
+    return ball[1] > rim[3]
+
+def is_made_shot(above_rim, below_rim, rim):
+    """
+    Args:
+    above_rim (cx, cy, frame)
+    below_rim (cx, cy, frame)
+    rim (x1, y1, x2, y2, frame)
+    """
+    x1, y1, x2 = rim[0], rim[1], rim[2]
+    cx1, cy1, cx2, cy2 = above_rim[0], above_rim[1], below_rim[0], below_rim[1]
+
+    m = (cy2-cy1)/(cx2-cx1)
+    b = cy1 - m*cx1
+    x = (y1 - b) / m
+
+    return x1 < x and x < x2
